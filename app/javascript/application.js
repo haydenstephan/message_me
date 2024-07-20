b4da1fd4596e9026a343b7fc040c46061299edcb
Hayden Charles Stephan <hayden.c.stephan@gmail.com>
2024-07-20 17:15:37 -0500

    Add auto-scrolling to chat window

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,13 +2,22 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-import "@hotwired/turbo-rails"
 import * as jquery from "jquery"
 import "semantic-ui"
+
+// scroll_bottom = function() {
+//   if ($('#messages').length > 0) {
+//     $('#messages').scrollTop($('#messages')[0].scrollHeight)
+//   }
+// }
 
 $(document).on('turbo:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  //scroll_bottom();
+  $('#messages').scrollTop($('#messages')[0].scrollHeight);
 })
+
+import "channels"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  #config.action_cable.alllowed_request_origins = ['https://messageme_test.herokuapp.com']
 end
